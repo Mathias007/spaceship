fe316d50e2996f5c381796e3cc652b65eb987539
Mathias007 <mathias.najlepszy@gmail.com>
2021-01-16 19:48:43 +0100

    handle creating and moving enemies, update style

--- js/Game.js
+++ js/Game.js
@@ -1,4 +1,5 @@
 import { Spaceship } from "./Spaceship.js";
+import { Enemy } from "./Enemy.js";
 
 class Game {
     #htmlElements = {
@@ -11,7 +12,11 @@ class Game {
         this.#htmlElements.container
     );
 
+    #enemies = [];
+    #enemiesInterval = null;
+
     #checkPositionInterval = null;
+    #createEnemyInterval = null;
 
     init() {
         this.#ship.init();
@@ -19,13 +24,53 @@ class Game {
     }
 
     #newGame() {
+        this.#enemiesInterval = 30;
+        this.#createEnemyInterval = setInterval(
+            () => this.#randomNewEnemy(),
+            1000
+        );
         this.#checkPositionInterval = setInterval(
             () => this.#checkPosition(),
             1
         );
     }
 
+    #randomNewEnemy() {
+        const randomNumber = Math.floor(Math.random() * 5) + 1;
+        randomNumber % 5
+            ? this.#createNewEnemy(
+                  this.#htmlElements.container,
+                  this.#enemiesInterval,
+                  "enemy"
+              )
+            : this.#createNewEnemy(
+                  this.#htmlElements.container,
+                  this.#enemiesInterval * 2,
+                  "enemy--big",
+                  3
+              );
+    }
+
+    #createNewEnemy(...params) {
+        const enemy = new Enemy(...params);
+
+        enemy.init();
+        this.#enemies.push(enemy);
+    }
+
     #checkPosition() {
+        this.#enemies.forEach((enemy, enemyIndex, enemiesArr) => {
+            const enemyPosition = {
+                top: enemy.element.offsetTop,
+                right: enemy.element.offsetLeft + enemy.element.offsetWidth,
+                bottom: enemy.element.offsetTop + enemy.element.offsetHeight,
+                left: enemy.element.offsetLeft,
+            };
+            if (enemyPosition.top > window.innerHeight) {
+                enemy.remove();
+                enemiesArr.splice(enemyIndex, 1);
+            }
+        });
         this.#ship.missiles.forEach((missile, missileIndex, missileArr) => {
             const missilePosition = {
                 top: missile.element.offsetTop,
